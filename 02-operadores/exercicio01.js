6116cd1636c3d80b54a0f949e5c7ae01b43a10f5
Alisson <alisson5051f@gmail.com>
2026-08-07 19:16:23 -0300

    Update exercicio01.js

--- 02-operadores/exercicio01.js
+++ 02-operadores/exercicio01.js
@@ -1,1 +1,7 @@
+let numero1 = 20;
+let numero2 = 5;
 
+console.log("Soma:", numero1 + numero2);
+console.log("Subtração:", numero1 - numero2);
+console.log("Multiplicação:", numero1 * numero2);
+console.log("Divisão:", numero1 / numero2);
