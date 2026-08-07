f8ac66892ca3d5a37c1d03fae24d4b8c6e185152
Alisson <alisson5051f@gmail.com>
2026-08-07 19:18:21 -0300

    Fix formatting issues in exercicio01.js

--- 02-operadores/exercicio01.js
+++ 02-operadores/exercicio01.js
@@ -5,4 +5,4 @@ console.log("Soma:", numero1 + numero2);
 console.log("Subtração:", numero1 - numero2);
 console.log("Multiplicação:", numero1 * numero2);
 console.log("Divisão:", numero1 / numero2);
-.
+
