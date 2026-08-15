665f2b464ce3d9efa56522cc2cb8f41005d9ebf9
Alisson <alisson5051f@gmail.com>
2026-08-15 17:39:50 -0300

    Calculate sum of even numbers in an array

--- 05-vetores/exercicio03.js
+++ 05-vetores/exercicio03.js
@@ -1,1 +1,11 @@
+let numeros = [1, 2, 3, 4, 5, 6, 7, 8];
 
+let soma = 0;
+
+for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] % 2 === 0) {
+        soma += numeros[i];
+    }
+}
+
+console.log("Soma dos pares:", soma);
