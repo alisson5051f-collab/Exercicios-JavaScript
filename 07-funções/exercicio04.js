4f8c1b10e30d89fdcfc30906737151021cf6cfae
Alisson <alisson5051f@gmail.com>
2026-08-15 17:42:58 -0300

    Add function to sum even numbers in an array

--- 07-funções/exercicio04.js
+++ 07-funções/exercicio04.js
@@ -1,1 +1,15 @@
+function somarPares(vetor) {
+    let soma = 0;
 
+    for (let i = 0; i < vetor.length; i++) {
+        if (vetor[i] % 2 === 0) {
+            soma += vetor[i];
+        }
+    }
+
+    return soma;
+}
+
+let numeros = [1, 2, 3, 4, 5, 6];
+
+console.log("Soma dos pares:", somarPares(numeros));
