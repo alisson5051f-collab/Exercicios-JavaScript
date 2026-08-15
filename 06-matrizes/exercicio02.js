bcbd7d19024b246fc4435ca42545cc162441c19a
Alisson <alisson5051f@gmail.com>
2026-08-15 17:40:18 -0300

    Update exercicio02.js

--- 06-matrizes/exercicio02.js
+++ 06-matrizes/exercicio02.js
@@ -1,1 +1,14 @@
+let matriz = [
+    [1, 2],
+    [3, 4]
+];
 
+let soma = 0;
+
+for (let i = 0; i < matriz.length; i++) {
+    for (let j = 0; j < matriz[i].length; j++) {
+        soma += matriz[i][j];
+    }
+}
+
+console.log("Soma:", soma);
